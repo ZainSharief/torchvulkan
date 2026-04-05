512631b193fc55298a4870e963bde97d7e8ff7a5
Zain Sharief <zain.sharief21@gmail.com>
2026-04-05 19:41:12 +0100

    fix: clear cache in device deletion to avoid deleting active mutex

--- src/vulkan/device_context.cpp
+++ src/vulkan/device_context.cpp
@@ -41,6 +41,8 @@ void DeviceContext::flush()
 
 DeviceContext::~DeviceContext() 
 {
+    cache.clearCache();
+
     if (commandPool != VK_NULL_HANDLE) device_table.vkDestroyCommandPool(device, commandPool, nullptr);
     if (allocator != VK_NULL_HANDLE) vmaDestroyAllocator(allocator);
     if (device != VK_NULL_HANDLE) device_table.vkDestroyDevice(device, nullptr);
