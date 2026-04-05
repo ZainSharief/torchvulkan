79a17125af1ddd9dd8304b226647af9754a25a00
Zain Sharief <zain.sharief21@gmail.com>
2026-04-05 16:50:26 +0100

    chore: initialize build system

---
+++ torchvulkan/__init__.py
@@ -0,0 +1,14 @@
+try: 
+    import torch
+except ImportError as e:
+    raise ImportError("PyTorch is required to use torchvulkan. Please install PyTorch and try again.") from e
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
+from . import _C
+
+torch.utils.rename_privateuse1_backend('vulkan')
+torch._register_device_module('vulkan', object())
