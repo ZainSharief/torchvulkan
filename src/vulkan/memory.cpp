7815330c69876856e10fd92f0dd72ef6f1c545e4
Zain Sharief <zain.sharief21@gmail.com>
2026-04-05 18:19:18 +0100

    feat: implement VulkanBuffer wrapper for vma allocations

--- src/vulkan/memory.cpp
+++ src/vulkan/memory.cpp
@@ -1,2 +1,43 @@
 #define VMA_IMPLEMENTATION 
 #include "memory.h" 
+
+VkResult VulkanBuffer::createBuffer(size_t size, MemoryUsage usage)
+{
+    size_ = size;
+    usage_ = usage;
+    VkBufferCreateInfo bufferInfo = {};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = size;
+    bufferInfo.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | // allow shader read/write this buffer
+                       VK_BUFFER_USAGE_TRANSFER_SRC_BIT |   // allow copy from this buffer
+                       VK_BUFFER_USAGE_TRANSFER_DST_BIT;    // allow copy to this buffer
+
+    VmaAllocationCreateInfo allocInfo = {};
+
+    switch (usage) 
+    {
+        case MemoryUsage::DEVICE_ONLY:
+            allocInfo.usage = VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE;
+            break;
+
+        case MemoryUsage::HOST_TO_DEVICE:
+            allocInfo.usage = VMA_MEMORY_USAGE_AUTO_PREFER_HOST;
+            allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | 
+                              VMA_ALLOCATION_CREATE_MAPPED_BIT;
+            break;
+
+        case MemoryUsage::DEVICE_TO_HOST:
+            allocInfo.usage = VMA_MEMORY_USAGE_AUTO_PREFER_HOST;
+            allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT | 
+                              VMA_ALLOCATION_CREATE_MAPPED_BIT;
+            break;
+    }
+
+    VkResult result = vmaCreateBuffer(allocator_, &bufferInfo, &allocInfo, &buffer_, &allocation_, &allocInfo_);    
+    if (result == VK_SUCCESS && usage != MemoryUsage::DEVICE_ONLY) mappedData_ = allocInfo_.pMappedData;
+    return result;
+}
+
+void VulkanBuffer::invalidate() { vmaInvalidateAllocation(allocator_, allocation_, 0, VK_WHOLE_SIZE); }
+void VulkanBuffer::flush() { vmaFlushAllocation(allocator_, allocation_, 0, VK_WHOLE_SIZE); }
+VulkanBuffer::~VulkanBuffer() { if (buffer_ != VK_NULL_HANDLE) vmaDestroyBuffer(allocator_, buffer_, allocation_); }
--- src/vulkan/memory.h
+++ src/vulkan/memory.h
@@ -1,2 +1,36 @@
 #pragma once
 #include "vk_mem_alloc.h"
+
+enum class MemoryUsage {
+    HOST_TO_DEVICE,
+    DEVICE_TO_HOST,
+    DEVICE_ONLY
+};
+
+class VulkanBuffer {
+public:
+    VulkanBuffer(VmaAllocator allocator) : allocator_(allocator) {};
+    ~VulkanBuffer();
+
+    // delete copy constructors
+    VulkanBuffer(const VulkanBuffer&) = delete;
+    VulkanBuffer& operator=(const VulkanBuffer&) = delete;
+
+    VkResult createBuffer(size_t size, MemoryUsage usage);
+    void* data() { return mappedData_; }
+    size_t size() const { return size_; }
+    MemoryUsage usage() const { return usage_; }
+    VkBuffer buffer() const { return buffer_; }
+
+    void invalidate();
+    void flush();
+
+private:
+    VmaAllocator allocator_;
+    VmaAllocation allocation_ = VK_NULL_HANDLE;
+    VmaAllocationInfo allocInfo_{};
+    VkBuffer buffer_ = VK_NULL_HANDLE;
+    size_t size_;
+    MemoryUsage usage_;
+    void* mappedData_ = nullptr;
+};
